f3f2071fb0ca38b80c721b0d34af478b1d97adf3
Goutham Vasireddi VLN <goutham.vasireddi@gmail.com>
2012-06-17 21:49:17 +0000

    Minor change

--- omod/src/main/java/org/openmrs/module/formfilter/web/controller/EditFormFilterController.java
+++ omod/src/main/java/org/openmrs/module/formfilter/web/controller/EditFormFilterController.java
@@ -7,7 +7,7 @@ import org.openmrs.Form;
 import org.openmrs.api.FormService;
 import org.openmrs.api.context.Context;
 
-import org.openmrs.module.formfilter.api.FormFilterService;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,10 +23,10 @@ public class EditFormFilterController  {
 	public void editFormFilter(ModelMap model,@RequestParam("formId") Integer formId) {
 		
 		FormService formService = Context.getFormService();
-		FormFilterService formFilterService=null;
+		//FormFilterService formFilterService=null;
 		//FormFilterService formFilterService =(FormFilterService)Context.getService(FormFilterService.class);
 		//Form form=formService.getForm(formId);
-		//FormFilter filter=formFilterService.getFormFilter(form);
+		model.addAttribute("form",formService.getForm(formId));
 		
 		
 	}
